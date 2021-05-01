739ae64dccff12fc158f9f1354a61b617941817d
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-01 22:54:12 +0200

    64. Create get handler and razor page

---
+++ SparkAuto/SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ServiceType ServiceType { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
