0b35a1df370844775bb8a2d412ca59c0f72fc29b
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-01 23:15:47 +0200

    67. Edit get and post handler

---
+++ SparkAuto/SparkAuto/Pages/ServiceTypes/Edit.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _context.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType is null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var serviceFromDb = await _context.ServiceType.FirstOrDefaultAsync(x => x.Id == ServiceType.Id);
+            serviceFromDb.Name = ServiceType.Name;
+            serviceFromDb.Price = ServiceType.Price;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
