fb3ec60215037029f36f7e69c833235a2390f418
Soultan Hatsijev <soultan_98@hotmail.com>
2021-05-01 16:25:00 +0200

    60. Index get handler

--- SparkAuto/SparkAuto/Pages/ServiceTypes/Index.cshtml.cs
+++ SparkAuto/SparkAuto/Pages/ServiceTypes/Index.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
 
 namespace SparkAuto.Pages.ServiceTypes
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<ServiceType> ServiceTypes { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ServiceTypes = await _context.ServiceType.ToListAsync();
+            return Page();
         }
     }
 }
